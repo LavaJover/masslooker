fad26fc21a6a1ff2f5b28f138eb17054b53c502a
bodya <lavajover@gmail.com>
2024-11-30 08:01:16 +0300

    refactor

--- MassLooker/config/config.py
+++ MassLooker/config/config.py
@@ -1,5 +1,11 @@
 import json
+from typing import Any
 
-def load_config(filename: str):
+
+def load_config(filename: str = 'config.json') -> Any:
     with open(filename, "r") as f:
-        return json.load()
+        return json.load(f)
+
+
+if __name__ == '__main__':
+    print(load_config('config.json'))
